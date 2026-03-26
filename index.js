afb7686f4fe492a7a7689e1a44d661cdd2d54ab2
Babanitza <Lukas.Bebnitz@t-online.de>
2026-03-26 11:48:23 +0100

    Neu

--- index.js
+++ index.js
@@ -47,6 +47,6 @@ app.post('/send-email', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => console.log("Server läuft auf Port " + PORT));
-
+alert(process.env.PORT);
