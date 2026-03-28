5a8b90a789a21c08758347cac4383188f2c4a2b9
Babanitza <Lukas.Bebnitz@t-online.de>
2026-03-28 20:11:57 +0100

    poport

--- index.js
+++ index.js
@@ -39,5 +39,5 @@ app.post("/sendmail", async (req, res) => {
   }
 });
 
-const PORT = railway.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server läuft auf Port " + PORT));
